c477fe15b8793d3cf31c2f06f1d881e54741da43
saumya <2saumyaray@gmail.com>
2019-09-27 13:19:51 +0530

    init

---
+++ functions.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Saumya Theme functions and definitions
+ *
+ * @link https://developer.wordpress.org/themes/basics/theme-functions/
+ *
+ * @package WordPress
+ * @subpackage Twenty_Nineteen
+ * @since 1.0.0
+ * 
+ */
+
+// ref: 
+// basic 	: https://themeshaper.com/2009/04/17/wordpress-child-theme-basics/
+// remove footer : https://www.wpbeginner.com/wp-themes/how-to-remove-the-powered-by-wordpress-footer-links/
+
+
+
+
